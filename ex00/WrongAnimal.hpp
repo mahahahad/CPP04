68841e00897ecb0a2bf6487c9181247467385d18
mahahahad <mahad.abd2003@gmail.com>
2025-03-01 15:00:18 +0400

    fix: remove `virtual` keyword to make incorrect

--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -11,8 +11,8 @@ class   WrongAnimal {
         WrongAnimal( const std::string& wrongAnimalType );
         WrongAnimal( const WrongAnimal& copy );
         WrongAnimal& operator=( const WrongAnimal& copy );
-        virtual ~WrongAnimal();
-        virtual void    makeSound( void ) const;
+        ~WrongAnimal();
+        void            makeSound( void ) const;
         std::string     getType( void ) const;
         void            setType( const std::string& newType );
     protected:
