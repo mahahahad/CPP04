f45a79fa0eac50c3c1c8cd32ea2a8816aaec92af
mahahahad <mahad.abd2003@gmail.com>
2025-03-01 15:00:32 +0400

    feat: restrict scope for cleaner code

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,24 +3,29 @@
 #include "WrongCat.hpp"
 
 int main() {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
+    {
 
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
-
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
+        
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); //will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+    
+        // Free allocated classes.
+        delete meta;
+        delete j;
+        delete i;
+    }
     // WrongAnimal class testing
-    const   WrongAnimal* wrongCat = new WrongCat();
-    wrongCat->makeSound();
-
-    // Free allocated classes.
-    delete meta;
-    delete j;
-    delete i;
-    delete wrongCat;
+    {
+        const   WrongAnimal* wrongCat = new WrongCat();
+        wrongCat->makeSound();
+        
+        delete wrongCat;
+    }
     return (0);
 }
